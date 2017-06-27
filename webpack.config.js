3308f938f657f412f9b52e70a0e59ef06e7485e6
Budi Arsana <gusdecool@gmail.com>
2017-06-27 10:36:52 +0800

    return package name to Hiccup

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build/'),
         filename: 'hiccup.js',
-        library: 'HiccupSdk',
+        library: 'Hiccup',
         libraryTarget: 'umd'
     }
 };
